fe70f8086a4f63089c2734c3d5a9133c51fdb625
root <root@ip-172-31-40-234.ap-south-1.compute.internal>
2026-07-07 11:30:37 +0000

    Initial Spring Boot application

---
+++ src/main/java/com/devops/project01/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.devops.project01.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "DevOps CI/CD Pipeline Success";
+    }
+}
